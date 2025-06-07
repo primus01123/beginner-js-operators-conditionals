4ad7e5fbf9a1e8509f9f948f584a0d4f15d7c95d
primus01123 <chimmeremzel@gmail.com>
2025-06-07 14:05:15 +0100

    voter-eligibility.js

---
+++ voter-eligibility.js
@@ -0,0 +1,19 @@
+// task6-voter-eligibility.js
+// Create two variables: age and isCitizen.
+// A person can vote only if they are 18 or older AND a citizen.
+// let age = 17;
+// let isCitizen = true;
+// Write your code here
+// If eligible, print "Eligible to vote"
+// Otherwise, print "Not eligible to vote"
+
+let age = 17
+let isCitizen = true
+
+if (age >= 18 && isCitizen) {
+    console.log("You are eligible to vote.")
+} else {
+    console.log("You are not eligible to vote.")
+}
+
+ //"Not eligible to vote"
