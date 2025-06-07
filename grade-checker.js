2390e346cd5b7197ae035d525cf90542f47e81c2
primus01123 <chimmeremzel@gmail.com>
2025-06-07 13:03:06 +0100

    grade-checker

---
+++ grade-checker.js
@@ -0,0 +1,26 @@
+// task3-grade-checker.js
+// Create a variable called score.
+// Use these rules to print the correct grade:
+// 90 and above → A
+// 80 to 89 → B
+// 70 to 79 → C
+// 60 to 69 → D
+// Less than 60 → F
+// let score = 85;
+// Write your code here
+// Print the correct grade based on the score
+
+let score = 85
+
+if(score >= 92){
+    console.log("Grade: A")
+}else if(score >= 83){
+    console.log("Grade: B")
+}else if(score >= 72){
+    console.log("Grade: C")
+}else if(score >= 61){
+    console.log("Grade: D")
+}else{
+    console.log("Grade: F")
+}
+// Grade B
