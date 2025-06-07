441149e91f94e40d33aa41e3ec89929d258b6bf3
primus01123 <chimmeremzel@gmail.com>
2025-06-07 12:37:39 +0100

    even-or-odd.j

---
+++ even-or-odd.js
@@ -0,0 +1,15 @@
+//  File: task1-even-or-odd.js
+// Create a variable called number.
+// Check if it is even or odd using the % operator.
+// let number = 7;
+// Write your code here
+// If number is even, print "Even"
+// If number is odd, print "Odd"
+
+let number = 9
+if (number % 2 === 0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+// Number = Odd
