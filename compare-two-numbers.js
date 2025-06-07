dd47d0d7e64306bd47183ab7e1f3d2c937f49e28
primus01123 <chimmeremzel@gmail.com>
2025-06-07 12:47:40 +0100

    compare-two-numbers.js

---
+++ compare-two-numbers.js
@@ -0,0 +1,23 @@
+//  task2-compare-two-numbers.js
+// Create two variables: a and b.
+// Check which number is bigger or if they are equal.
+// let a = 10;
+// let b = 20;
+
+// Write your code here
+// If a is bigger, print "a is greater"
+// If b is bigger, print "b is greater"
+// If they are equal, print "Both are equal"
+
+let a = 10;
+let b = 20;
+
+if (a > b) {
+  console.log("a is greater than b");
+} else if (a < b) {
+  console.log("b is greater than a");
+} else {
+  console.log("a and b are equal");
+}
+
+// b is greater than a
