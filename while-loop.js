734c680386d328ae0e8887abd66ae36962c720f0
primus01123 <chimmeremzel@gmail.com>
2025-06-11 13:47:49 +0100

    while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,19 @@
+// while(ture condition){
+//     execute this block until the condition becomes false
+// }
+let x = 7
+
+// while(x == 7){
+//     console.log("x is 7")
+
+// or
+
+// while(true){
+//     console.log("x is 7")
+// }    
+
+while(x < 12){
+    console.log("x is 7")
+    x = x + 1
+    console.log(x)
+}
